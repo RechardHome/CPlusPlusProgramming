1f7eb5ef27918a810ef924df5d8af5a84577ed8a
jiliszc <jili_baoer.std@163.com>
2017-12-16 19:26:58 +0800

    Add chapter5 thread test

---
+++ Chapter5/TestThread.cpp
@@ -0,0 +1,23 @@
+#include <thread>
+#include <iostream>
+
+using namespace std;
+
+void f() {cout << "Hello";}
+
+struct F{
+    void operator()(){cout << "Parallel World!\n";}
+};
+
+void user() {
+    thread t1 {f};
+    thread t2 {F()};
+
+    t1.join();	// wait t1 process
+    t2.join();
+}
+
+int main(int argc, char** argv) {
+    user();
+    return 0;
+}
